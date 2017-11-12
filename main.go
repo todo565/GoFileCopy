b56e0700765bac21eb5ac25712b63e23ef1c6f23
todo565 <565135@gmail.com>
2017-11-13 01:13:16 +0900

    v1.1.0 DB保存機能追加

--- main.go
+++ main.go
@@ -7,8 +7,9 @@ import (
 	"flag"
 	"io/ioutil"
 	"os"
-	"path"
 	"io"
+	"src/github.com/jinzhu/gorm"
+	_"src/github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 var (
@@ -16,11 +17,19 @@ var (
 	dPath = flag.String("d","c:/","destnationFilePath")
 	ext = flag.String("e",".exe","fileExtension")
 )
+
+type Filelist struct {
+	gorm.Model
+	FileName string
+	Date string
+}
+
 func main(){
 	flag.Parse()
 	fmt.Println("元dir " + *oPath)
 	fmt.Println("先dir " + *dPath)
 	fmt.Println("拡張子 " + *ext)
+
 
 	t := time.NewTicker(60 * time.Second)
 	for {
@@ -28,7 +37,8 @@ func main(){
 		case <-t.C:
 			fileList := dirwalk(*oPath,*dPath,*ext)
 			if len(fileList) != 0 {
-				fmt.Println(fileList)
+				//fmt.Println(fileList)
+				writeDB(fileList)
 			}
 		}
 	}
@@ -44,11 +54,12 @@ func dirwalk(pOPath string,pDPath string,pExt string) []string {
 
 	var paths []string
 	for _, file := range files {
-		if path.Ext(file.Name()) ==  pExt{
+		fmt.Println(file.Name() + " : " + filepath.Ext(file.Name()))
+		if filepath.Ext(file.Name()) ==  pExt{
 			ofile := filepath.Join(pOPath,file.Name())
 			dfile := filepath.Join(pDPath,file.Name())
 
-			FileCopy(ofile,dfile)
+			fileCopy(ofile,dfile)
 
 			paths = append(paths, filepath.Join(pOPath, file.Name()))
 		}
@@ -57,7 +68,7 @@ func dirwalk(pOPath string,pDPath string,pExt string) []string {
 }
 
 //ファイルコピー
-func FileCopy(pOfile string,pDfile string){
+func fileCopy(pOfile string,pDfile string){
 	oldFile,err := os.Open(pOfile)
 	if err != nil{
 		panic(err)
@@ -73,4 +84,19 @@ func FileCopy(pOfile string,pDfile string){
 	}
 	oldFile.Close()
 	os.Remove(pOfile)
+}
+
+func writeDB(pFileList []string){
+	db,err := gorm.Open("sqlite3","List.sqlite3")
+	if err != nil{
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	db.AutoMigrate(&Filelist{})
+
+	for _,file := range pFileList{
+		fmt.Println(file)
+		db.Create(&Filelist{FileName:file,Date:time.Now().Format("2006/01/02")})
+	}
 }
